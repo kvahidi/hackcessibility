6ffa4d9f371b48c42d42c023c7d736a6f355b243
qbielka@sfu.ca <qbielka@sfu.ca>
2018-12-01 14:42:47 -0800

    PublicSpaceActivity now empty

--- app/src/main/java/com/mlh2018/hackcessibility/PublicSpaceActivity.java
+++ app/src/main/java/com/mlh2018/hackcessibility/PublicSpaceActivity.java
@@ -13,14 +13,6 @@ public class PublicSpaceActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_public_space);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
