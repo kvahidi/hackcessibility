798bb59c042c24ab263633414d6354f5bbd77e4d
hilal <hasmat@sfu.ca>
2018-12-01 16:16:46 -0800

    added incident report activity

---
+++ app/src/main/java/com/mlh2018/hackcessibility/IncidentReportActivity.java
@@ -0,0 +1,13 @@
+package com.mlh2018.hackcessibility;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IncidentReportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_incident_report);
+    }
+}
