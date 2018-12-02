d6223359416b2f0b88cb19f90d71563a2f59e658
qbielka@sfu.ca <qbielka@sfu.ca>
2018-12-01 18:59:08 -0800

    Results Activity Finished commit

--- app/src/main/java/com/mlh2018/hackcessibility/ResultsActivity.java
+++ app/src/main/java/com/mlh2018/hackcessibility/ResultsActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RatingBar;
@@ -25,8 +26,8 @@ public class ResultsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
-        //thePlace=serverParser();
-        //updateList();
+        thePlace=serverParser();
+        updateList();
     }
 
     private void updateList() {
@@ -37,12 +38,17 @@ public class ResultsActivity extends AppCompatActivity {
         LinearLayout seating = findViewById(R.id.seating_layout);
         LinearLayout washrooms = findViewById(R.id.washrooms_layout);
 
-        updateData(ramp);
+        updateRampData(ramp);
+        updateElevatorData(elevator);
+        updateDoorData(doors);
+        updateParkingData(parking);
+        updateSeatingData(seating);
+        updateWashroomData(washrooms);
 
     }
 
-
-    private void updateData(LinearLayout slot) {
+    //updates
+    private void updateRampData(LinearLayout slot){
         int goods=0;
         int bads=0;
         int nones=0;
@@ -55,26 +61,110 @@ public class ResultsActivity extends AppCompatActivity {
                 nones++;
             }
         }
-        if ( ((nones + bads) >= goods) && (bads < nones) ){
-            //ToDo other none
-        }else {
-            //slot
-            RatingBar bar = new RatingBar(this);
-            bar.setNumStars(5);
+        update(slot, goods, bads, nones);
+    }
+    private void updateElevatorData(LinearLayout slot){
+        int goods=0;
+        int bads=0;
+        int nones=0;
+        for (Features features: thePlace.getFeatures()){
+            if(features.getElevator().getEvaluation()==Evaluation.GOOD){
+                goods++;
+            }if(features.getElevator().getEvaluation()==Evaluation.BAD){
+                bads++;
+            }if(features.getElevator().getEvaluation()==Evaluation.NOT_HERE){
+                nones++;
+            }
+        }
+        update(slot, goods, bads, nones);
+    }
+    private void updateDoorData(LinearLayout slot){
+        int goods=0;
+        int bads=0;
+        int nones=0;
+        for (Features features: thePlace.getFeatures()){
+            if(features.getAutomaticDoor().getEvaluation()==Evaluation.GOOD){
+                goods++;
+            }if(features.getAutomaticDoor().getEvaluation()==Evaluation.BAD){
+                bads++;
+            }if(features.getAutomaticDoor().getEvaluation()==Evaluation.NOT_HERE){
+                nones++;
+            }
+        }
+        update(slot, goods, bads, nones);
+    }
+    private void updateParkingData(LinearLayout slot){
+        int goods=0;
+        int bads=0;
+        int nones=0;
+        for (Features features: thePlace.getFeatures()){
+            if(features.getParking().getEvaluation()==Evaluation.GOOD){
+                goods++;
+            }if(features.getParking().getEvaluation()==Evaluation.BAD){
+                bads++;
+            }if(features.getParking().getEvaluation()==Evaluation.NOT_HERE){
+                nones++;
+            }
+        }
+        update(slot, goods, bads, nones);
+    }
+    private void updateSeatingData(LinearLayout slot){
+        int goods=0;
+        int bads=0;
+        int nones=0;
+        for (Features features: thePlace.getFeatures()){
+            if(features.getSeating().getEvaluation()==Evaluation.GOOD){
+                goods++;
+            }if(features.getSeating().getEvaluation()==Evaluation.BAD){
+                bads++;
+            }if(features.getSeating().getEvaluation()==Evaluation.NOT_HERE){
+                nones++;
+            }
+        }
+        update(slot, goods, bads, nones);
+    }
+    private void updateWashroomData(LinearLayout slot){
+        int goods=0;
+        int bads=0;
+        int nones=0;
+        for (Features features: thePlace.getFeatures()){
+            if(features.getWashrooms().getEvaluation()==Evaluation.GOOD){
+                goods++;
+            }if(features.getWashrooms().getEvaluation()==Evaluation.BAD){
+                bads++;
+            }if(features.getWashrooms().getEvaluation()==Evaluation.NOT_HERE){
+                nones++;
+            }
+        }
+        update(slot, goods, bads, nones);
+    }
 
+    private void update(LinearLayout slot, int goods, int bads, int nones) {
+
+        if ( ((nones + bads) >= goods) && (bads < nones) ){
+            TextView none = new TextView(this);
+            none.setText(R.string.none);
+            none.setLayoutParams(new LinearLayout.LayoutParams
+                    (LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            slot.addView(none);
+        }else {
+
+            // create bar
+            final RatingBar ratingBar = new RatingBar(this);
+            // set bar
+            ratingBar.setStepSize((float) 0.5);
+            ratingBar.setNumStars(3);
+            ratingBar.setMax(3);
+            ratingBar.setRating(((float)goods)/(bads+goods)*3);
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams
+                    (LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            ratingBar.setLayoutParams(layoutParams);
+
+            slot.addView(ratingBar);
         }
 
     }
 
-    private void look(){
-        LinearLayout ll = (LinearLayout)findViewById(R.id.ramp_layout);
-
-        Button btn = new Button(this);
-        btn.setText("Manual Add");
-        btn.setLayoutParams(new LinearLayout.LayoutParams
-                (LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-        ll.addView(btn);
-    }
 
     // TODO get rid of garbage Data
     private AccessiblePlace serverParser(){
@@ -113,7 +203,7 @@ public class ResultsActivity extends AppCompatActivity {
 
 
     public Features garbageData2(){
-        FeatureEvaluation ramp = new FeatureEvaluation("Ramp", Evaluation.BAD);
+        FeatureEvaluation ramp = new FeatureEvaluation("Ramp", Evaluation.GOOD);
         FeatureEvaluation elevator = new FeatureEvaluation("Elevator", Evaluation.BAD);
         FeatureEvaluation parking = new FeatureEvaluation("Parking", Evaluation.BAD);
         FeatureEvaluation automaticDoor = new FeatureEvaluation ("AutomaticDoor", Evaluation.BAD);
@@ -126,7 +216,7 @@ public class ResultsActivity extends AppCompatActivity {
 
     public Features garbageData3(){
         FeatureEvaluation ramp =            new FeatureEvaluation("Ramp", Evaluation.GOOD);
-        FeatureEvaluation elevator =        new FeatureEvaluation("Elevator", Evaluation.GOOD);
+        FeatureEvaluation elevator =        new FeatureEvaluation("Elevator", Evaluation.BAD);
         FeatureEvaluation parking =         new FeatureEvaluation("Parking", Evaluation.GOOD);
         FeatureEvaluation automaticDoor =   new FeatureEvaluation ("AutomaticDoor", Evaluation.GOOD);
         FeatureEvaluation seating =         new FeatureEvaluation ("Seating", Evaluation.GOOD);
